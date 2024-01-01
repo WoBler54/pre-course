56c7152a3baac154b039c7ed4dfc138fe7e98b34
Yahor Trafimau <19rabigu@gmail.com>
2024-01-01 13:35:17 +0100

    lesson 1 complite

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,3 @@
-// code
+let randomNumber = Math.floor(Math.random() * 100);
+
+randomNumber % 2 == 0 ? console.log(randomNumber , ' :Это четное число') : console.log(randomNumber , '  :Это нечетное число') ;
