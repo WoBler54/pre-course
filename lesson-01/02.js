1531f7e1814791a6e1be94ff3daf391472bb66eb
Yahor Trafimau <19rabigu@gmail.com>
2024-01-01 18:10:17 +0100

    corrected-erros

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,3 +1,3 @@
 let randomNumber = Math.floor(Math.random() * 100);
 
-randomNumber % 2 == 0 ? console.log(randomNumber , ' :Это четное число') : console.log(randomNumber , '  :Это нечетное число') ;
+randomNumber % 2 == 0 ? console.log(randomNumber , `«${randomNumber} :Это четное число»` ) : console.log( `«${randomNumber} :Это нечетное число»`) ;
